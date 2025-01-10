8f9cb62d577c3e8d5184a09d55a6c7190b2b930c
evaldo <evaaldormj@gmail.com>
2025-01-10 19:16:42 -0300

    Feat: Adicionar geração e validação de QR code

---
+++ 2FA/Controllers/AuthenticationController.cs
@@ -0,0 +1,38 @@
+﻿using Google.Authenticator;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+
+namespace _2FA.Controllers
+{
+    [ApiController]
+    [Route("/auth")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly TwoFactorAuthenticator _tfa;
+        private readonly ILogger<AuthenticationController> _logger;
+
+        public AuthenticationController(TwoFactorAuthenticator tfa, ILogger<AuthenticationController> logger)
+        {
+            _tfa = tfa;
+            _logger = logger;
+        }
+
+        [HttpGet("generateQr/{email}")]
+        public ActionResult<string> GenerateQR(string email)
+        {
+            string key = Guid.NewGuid().ToString().Replace("-","").Substring(0,10);
+            SetupCode setupInfo = _tfa.GenerateSetupCode("2FA DO EVALDO", email, key, false, 3);
+
+            _logger.LogInformation(key);
+
+            return Ok(setupInfo.QrCodeSetupImageUrl);
+        }
+
+        [HttpPost("validateCode")]
+        public ActionResult<bool> GenerateQR(string code, string key)
+        {
+            return _tfa.ValidateTwoFactorPIN(code, key);
+        }
+    }
+}
